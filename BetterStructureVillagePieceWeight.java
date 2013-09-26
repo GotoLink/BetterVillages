245590729f5810eb59137c7bf9c50c3a6583b313
GotoLink <gotolinkminecraft@gmail.com>
2013-09-26 17:29:19 +0200

    First commit

---
+++ BetterStructureVillagePieceWeight.java
@@ -0,0 +1,21 @@
+package bettervillages;
+
+import net.minecraft.world.gen.structure.StructureVillagePieceWeight;
+
+public class BetterStructureVillagePieceWeight extends StructureVillagePieceWeight{
+
+	public BetterStructureVillagePieceWeight(Class par1Class, int par2, int par3) {
+		super(par1Class, par2, par3);
+	}
+	@Override
+	public boolean canSpawnMoreVillagePiecesOfType(int par1)
+    {
+        return this.villagePiecesLimit == 10 || this.villagePiecesSpawned < this.villagePiecesLimit;
+    }
+
+	@Override
+    public boolean canSpawnMoreVillagePieces()
+    {
+        return this.villagePiecesLimit == 10 || this.villagePiecesSpawned < this.villagePiecesLimit;
+    }
+}
---
+++ ComponentBetterVillageTorch.java
@@ -0,0 +1,56 @@
+package bettervillages;
+
+import java.util.List;
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.world.World;
+import net.minecraft.world.gen.structure.ComponentVillageStartPiece;
+import net.minecraft.world.gen.structure.ComponentVillageTorch;
+import net.minecraft.world.gen.structure.StructureBoundingBox;
+
+public class ComponentBetterVillageTorch extends ComponentVillageTorch{
+
+	private int averageGroundLevel = -1;
+	public ComponentBetterVillageTorch(ComponentVillageStartPiece par1ComponentVillageStartPiece,
+			int par2, Random par3Random, StructureBoundingBox par4StructureBoundingBox, int par5) {
+		super(par1ComponentVillageStartPiece, par2, par3Random,
+				par4StructureBoundingBox, par5);
+	}
+	public static Object getTorch(ComponentVillageStartPiece startPiece, List pieces, Random random, int p1, int p2, int p3, int p4, int p5){
+		StructureBoundingBox structureboundingbox = func_74904_a(startPiece, pieces, random, p1, p2, p3, p4);
+	
+	    if (structureboundingbox != null)
+	    {
+	        return new ComponentBetterVillageTorch(startPiece, p5, random, structureboundingbox, p4);
+	    }
+	    else
+	    {
+	        return null;
+	    }
+	}
+    
+	@Override
+	public boolean addComponentParts(World par1World, Random par2Random, StructureBoundingBox par3StructureBoundingBox)
+	{
+	    if (this.averageGroundLevel < 0)
+	    {
+	        this.averageGroundLevel = this.getAverageGroundLevel(par1World, par3StructureBoundingBox);
+	
+	        if (this.averageGroundLevel < 0)
+	        {
+	            return true;
+	        }
+	
+	        this.boundingBox.offset(0, this.averageGroundLevel - this.boundingBox.maxY + 4 - 1, 0);
+	    }
+	
+	    //NetherFenceAndGlowstone
+	    this.fillWithBlocks(par1World, par3StructureBoundingBox, 0, 0, 0, 0, 3, 0, 0, 0, false);
+	    this.placeBlockAtCurrentPosition(par1World, Block.netherFence.blockID, 0, 1, 0, 0, par3StructureBoundingBox);
+	    this.placeBlockAtCurrentPosition(par1World, Block.netherFence.blockID, 0, 1, 1, 0, par3StructureBoundingBox);
+	    this.placeBlockAtCurrentPosition(par1World, Block.netherFence.blockID, 0, 1, 2, 0, par3StructureBoundingBox);
+	    this.placeBlockAtCurrentPosition(par1World, Block.glowStone.blockID, 0, 1, 3, 0, par3StructureBoundingBox);
+	    return true;
+	}
+}
