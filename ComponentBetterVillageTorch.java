ac9c60a5be61188b44b914fd6aa796dc828d8e1e
GotoLink <gotolinkminecraft@gmail.com>
2013-10-14 20:05:21 +0200

    Formatting

--- ComponentBetterVillageTorch.java
+++ ComponentBetterVillageTorch.java
@@ -9,50 +9,38 @@ import net.minecraft.world.gen.structure.ComponentVillageStartPiece;
 import net.minecraft.world.gen.structure.ComponentVillageTorch;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
 
-public class ComponentBetterVillageTorch extends ComponentVillageTorch{
+public class ComponentBetterVillageTorch extends ComponentVillageTorch {
+	public ComponentBetterVillageTorch() {
+	}
 
-	public ComponentBetterVillageTorch(){
+	public ComponentBetterVillageTorch(ComponentVillageStartPiece par1ComponentVillageStartPiece, int par2, Random par3Random, StructureBoundingBox par4StructureBoundingBox, int par5) {
+		super(par1ComponentVillageStartPiece, par2, par3Random, par4StructureBoundingBox, par5);
 	}
-	
-	public ComponentBetterVillageTorch(ComponentVillageStartPiece par1ComponentVillageStartPiece,
-			int par2, Random par3Random, StructureBoundingBox par4StructureBoundingBox, int par5) {
-		super(par1ComponentVillageStartPiece, par2, par3Random,
-				par4StructureBoundingBox, par5);
-	}
-	public static Object getTorch(ComponentVillageStartPiece startPiece, List pieces, Random random, int p1, int p2, int p3, int p4, int p5){
+
+	public static Object getTorch(ComponentVillageStartPiece startPiece, List pieces, Random random, int p1, int p2, int p3, int p4, int p5) {
 		StructureBoundingBox structureboundingbox = func_74904_a(startPiece, pieces, random, p1, p2, p3, p4);
-	
-	    if (structureboundingbox != null)
-	    {
-	        return new ComponentBetterVillageTorch(startPiece, p5, random, structureboundingbox, p4);
-	    }
-	    else
-	    {
-	        return null;
-	    }
+		if (structureboundingbox != null) {
+			return new ComponentBetterVillageTorch(startPiece, p5, random, structureboundingbox, p4);
+		} else {
+			return null;
+		}
 	}
-    
+
 	@Override
-	public boolean addComponentParts(World par1World, Random par2Random, StructureBoundingBox par3StructureBoundingBox)
-	{
-	    if (this.field_143015_k < 0)
-	    {
-	        this.field_143015_k = this.getAverageGroundLevel(par1World, par3StructureBoundingBox);
-	
-	        if (this.field_143015_k < 0)
-	        {
-	            return true;
-	        }
-	
-	        this.boundingBox.offset(0, this.field_143015_k - this.boundingBox.maxY + 4 - 1, 0);
-	    }
-	
-	    //NetherFenceAndGlowstone
-	    this.fillWithBlocks(par1World, par3StructureBoundingBox, 0, 0, 0, 0, 3, 0, 0, 0, false);
-	    this.placeBlockAtCurrentPosition(par1World, Block.netherFence.blockID, 0, 1, 0, 0, par3StructureBoundingBox);
-	    this.placeBlockAtCurrentPosition(par1World, Block.netherFence.blockID, 0, 1, 1, 0, par3StructureBoundingBox);
-	    this.placeBlockAtCurrentPosition(par1World, Block.netherFence.blockID, 0, 1, 2, 0, par3StructureBoundingBox);
-	    this.placeBlockAtCurrentPosition(par1World, Block.glowStone.blockID, 0, 1, 3, 0, par3StructureBoundingBox);
-	    return true;
+	public boolean addComponentParts(World par1World, Random par2Random, StructureBoundingBox par3StructureBoundingBox) {
+		if (this.field_143015_k < 0) {
+			this.field_143015_k = this.getAverageGroundLevel(par1World, par3StructureBoundingBox);
+			if (this.field_143015_k < 0) {
+				return true;
+			}
+			this.boundingBox.offset(0, this.field_143015_k - this.boundingBox.maxY + 4 - 1, 0);
+		}
+		//NetherFenceAndGlowstone
+		this.fillWithBlocks(par1World, par3StructureBoundingBox, 0, 0, 0, 0, 3, 0, 0, 0, false);
+		this.placeBlockAtCurrentPosition(par1World, Block.netherFence.blockID, 0, 1, 0, 0, par3StructureBoundingBox);
+		this.placeBlockAtCurrentPosition(par1World, Block.netherFence.blockID, 0, 1, 1, 0, par3StructureBoundingBox);
+		this.placeBlockAtCurrentPosition(par1World, Block.netherFence.blockID, 0, 1, 2, 0, par3StructureBoundingBox);
+		this.placeBlockAtCurrentPosition(par1World, Block.glowStone.blockID, 0, 1, 3, 0, par3StructureBoundingBox);
+		return true;
 	}
 }
