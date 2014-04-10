150985eed82fc855f303b346f2bf9640be59adde
GotoLink <gotolinkminecraft@gmail.com>
2014-04-10 15:59:51 +0200

    Added Javadoc

--- bettervillages/ComponentBetterVillageTorch.java
+++ bettervillages/ComponentBetterVillageTorch.java
@@ -8,8 +8,12 @@ import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
 import net.minecraft.world.gen.structure.StructureVillagePieces;
 
+/**
+ * New torch component for villages, using nether brick fences and glowstone blocks
+ */
 public class ComponentBetterVillageTorch extends StructureVillagePieces.Torch {
 	public ComponentBetterVillageTorch() {
+        super();
 	}
 
 	public ComponentBetterVillageTorch(StructureVillagePieces.Start par1ComponentVillageStartPiece, int par2, Random par3Random, StructureBoundingBox par4StructureBoundingBox, int par5) {
@@ -25,7 +29,7 @@ public class ComponentBetterVillageTorch extends StructureVillagePieces.Torch {
 			}
 			this.boundingBox.offset(0, this.field_143015_k - this.boundingBox.maxY + 4 - 1, 0);
 		}
-		//NetherFenceAndGlowstone
+		//NetherFence and Glowstone
 		this.fillWithBlocks(par1World, par3StructureBoundingBox, 0, 0, 0, 0, 3, 0, Blocks.air, Blocks.air, false);
 		this.placeBlockAtCurrentPosition(par1World, Blocks.nether_brick_fence, 0, 1, 0, 0, par3StructureBoundingBox);
 		this.placeBlockAtCurrentPosition(par1World, Blocks.nether_brick_fence, 0, 1, 1, 0, par3StructureBoundingBox);
@@ -34,6 +38,10 @@ public class ComponentBetterVillageTorch extends StructureVillagePieces.Torch {
 		return true;
 	}
 
+    /**
+     * Construction method for this component, called through reflection
+     * @return an instance of this, if successfully found
+     */
 	public static ComponentBetterVillageTorch getTorch(StructureVillagePieces.Start startPiece, List<?> pieces, Random random, int p1, int p2, int p3, int p4, int p5) {
 		StructureBoundingBox structureboundingbox = func_74904_a(startPiece, pieces, random, p1, p2, p3, p4);
 		if (structureboundingbox != null) {
